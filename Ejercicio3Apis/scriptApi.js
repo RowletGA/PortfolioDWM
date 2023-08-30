c856c60217f91ac4cab051c017e451a79a5d800d
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2023-08-30 20:33:58 -0300

    cambios

--- Ejercicio3Apis/scriptApi.js
+++ Ejercicio3Apis/scriptApi.js
@@ -32,24 +32,61 @@ const searchPokemon = event => {
   event.preventDefault();
   const { value } = event.target.pokemon;
   fetch	(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
-  .then(data => data.json())
-  .then(response => renderPokemonData(response))
-}
+    .then(data => data.json())
+    .then(response => renderPokemonData(response))
+    .catch(err => renderNotFound());
+  }
 const renderPokemonData = data => {
   const sprite = data.sprites.front_default;
   const { stats, types } = data;
  // console.log(data);
- pokeName.textContent = data.name;
- pokeImg.setAttribute('src', sprite);
- pokeId.textContent = `Nº ${data.id}`;
-
+  pokeName.textContent = data.name;
+  pokeImg.setAttribute('src', sprite);
+  pokeId.textContent = `Nº ${data.id}`;
   setCardColor(types);
   renderPokemonTypes(types);
   renderPokemonStats(stats);
 }
 const setCardColor = types => {
+  const colorOne = typeColors[types[0].type.name];
+  const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+  pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+  pokeImg.style.backgroundSize = '5px 5px';
   
 }
+const renderPokemonTypes = types => {
+  pokeTypes.innerHTML = '';
+  types.forEach(type => {
+    const typeTextElement = document.createElement('div');
+    typeTextElement.style.color = typeColors[type.type.name];
+    typeTextElement.textContent = type.type.name;
+    pokeTypes.appendChild(typeTextElement);
+  });
+
+  const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+      const statElement = document.createElement('div');
+      const statElementName = document.createElement('div');
+      const statElementAmount = document.createElement('div');
+      statElementName.textContent = stat.stat.name;
+      statElementAmount.textContent = stat.base_stat;
+      statElement.appendChild(statElementName);
+      statElement.appendChild(statElementAmount);
+      pokeStats.appendChild(statElement);
+    });
+  }
+  const renderNotFound = () => {
+    pokeName.textContent = 'Pokemon no encontrado';
+    pokeImg.setAttribute('src', 'https://i.imgur.com/vuW5qoF.png');
+    pokeImg.style.background = '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
+}
+}
+
+  
 
 
 /*Esta funcion saca un numero aleatorio entre 1 y 898 el cual usa para referenciar
@@ -58,7 +95,7 @@ const setCardColor = types => {
 
 function loadRandomPokemonInfo() {
     // Generar un número aleatorio entre 1 y 898 (cantidad de Pokémon válidos)
-    const randomPokemonId = Math.floor(Math.random() * 898) + 1;
+    const randomPokemonId = Math.floor(Math.random() * 1010) + 1;
     const url = `https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`;
 
     $.ajax({
